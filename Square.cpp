5f1a082f82282bb3b3cc4e64f44c675d147199ac
LirazBalas <118938031+LirazBalas1@users.noreply.github.com>
2024-10-06 23:24:08 +0300

    Update Square.cpp

--- Square.cpp
+++ Square.cpp
@@ -3,10 +3,13 @@
 //lbalad13@gmail.com
 #include "Square.hpp"
 
+// Constructor initializes the square's name
 Square::Square(const std::string& squareName) : name(squareName) {}
 
+// Getter for the square's name
 std::string Square::getName() const {
     return name;
 }
 
+// Virtual destructor to ensure cleanup in derived classes
 Square::~Square() {}
